b991d67378e28b1f4c5e661115de748ccb3b5925
sathvik-ujwal <sathvikmu04@gmail.com>
2025-08-22 19:28:02 +0530

    data generation helper functions

---
+++ DATA GENERATION/dataset/Taxes/data.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, "ambiguous.json"), "r") as f:
+    ambiguous = json.load(f)
+
+with open(os.path.join(base_dir, "specific.json"), "r") as f:
+    specific = json.load(f)
+
+data = {
+    "ambiguous": ambiguous,
+    "specific": specific
+}
